2db1d03270c84b05fb57e2a3c5d1bcdf39fdf2bc
PAYVISION\e.perez <e.perez@payvision.com>
2017-10-06 09:00:04 +0200

    Almost done visitor but remains json

--- Behavioral/Visitor/TaskManagerReport/TaskManagerReport/JSONReport.cs
+++ Behavioral/Visitor/TaskManagerReport/TaskManagerReport/JSONReport.cs
@@ -15,25 +15,61 @@ namespace TaskManagerReport
         public void Visit(Epic epic)
         {
             //Console.WriteLine("{0}", epic.Display());
-            var display = "{type: epic,description: epic.name,features: [";
+
+            var display = "{\n type:”epic”,\n description: “Epic1 \n";
             Console.WriteLine("{0}", display);
         }
 
         public void Visit(Feature task)
         {
-            var display = "{type: epic,description: epic.name,features: [";
+            //var display = "{type: epic,description: epic.name,features: [";
+            var display = "\t features: [\n { type: feature,\n description:" + task.name + "By: Warriors";
             Console.WriteLine("{0}", display);
         }
 
         public void Visit(US task)
         {
-            var display = "{type: epic,description: epic.name,features: [";
+            //var display = "{type: epic,description: epic.name,features: [";
+            var display = "UserHistories: [{\n"
+                          + "type: UserHistory,"
+                          + "description: US1,"
+                          + "historyPoints: 5"
+                          + "tasks:["
+                          + "{"
+                          + "type:Task,"
+                          + "description: T1,"
+                          + "hours4"
+                          + "}"
+                          + "]"
+                          + "}";
+            //{
+            //    type:UserHistory,
+            //    description: US2,
+            //    historyPoints: 3
+            //    tasks:[
+            //    {
+            //        type:Task,
+            //        description: T2,
+            //        hours3
+            //    }
+            //    ]
+            //}
+
+
+            //]";
+
             Console.WriteLine("{0}", display);
         }
 
         public void Visit(TeamTask task)
         {
-            var display = "{type: epic,description: epic.name,features: [";
+            var display = "tasks:["
+            + "{"
+              + "type:Task,"
+              + "description: T2,"
+              + "hours3"
+            + "}"
+            + "]";
             Console.WriteLine("{0}", display);
         }
     }
--- Behavioral/Visitor/TaskManagerReport/TaskManagerReport/Program.cs
+++ Behavioral/Visitor/TaskManagerReport/TaskManagerReport/Program.cs
@@ -23,15 +23,21 @@ namespace TaskManagerReport
             var f1 = new Feature("Equipo WARRIOR", "Feature1");
             f1.Add(us1);
             f1.Add(us2);
-           
+
+            var f2 = new Feature("Equipo WARRIOR2", "Feature2");
+            f2.Add(us1);
+            f2.Add(us2);
 
             var epic1 = new Epic(100, "Epic1");
             epic1.Add(f1);
 
+            var epic2 = new Epic(200, "Epic2");
+            epic2.Add(f2);
             //Store tasks
             teamWork.Add(epic1);
+            teamWork.Add(epic2);
 
-            
+
             foreach (var element in teamWork)
             {               
                element.Accept(textReport);
